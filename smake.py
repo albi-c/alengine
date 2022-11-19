81be36a19f1ec690f637500ad34b58f2599d3047
albi-c <albert.csontos@gmail.com>
2022-11-19 15:14:39 +0100

    fixed smake configuration

--- smake.py
+++ smake.py
@@ -1,12 +1,12 @@
 from smake import Smake
 
-Smake.library("lib", "lib")
-Smake.library("alengine", "alengine", ["lib"])
+Smake.library("lib", ["lib"])
+Smake.library("alengine", ["alengine"], ["lib"])
 
 Smake.extern_library("GL")
 Smake.extern_library("glfw")
 
-Smake.executable("test_game", "test/src", ["lib", "alengine", "GL", "glfw"])
+Smake.executable("test_game", ["test/src"], ["lib", "alengine", "GL", "glfw"])
 
 Smake.run("test_game")
 
